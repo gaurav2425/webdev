273d82c8a3bd089d0eeef89f92f673f12e421039
SarbrajMarway <ssm1117@ic.ac.uk>
2020-10-04 19:33:20 +0100

    completed login, logout and registration pages

--- webdev/urls.py
+++ webdev/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 
@@ -30,6 +31,7 @@ urlpatterns = [
     url('about/', views.about, name='about'),
     url('results/', views.results, name='results'),
     url('alevelbio/', views.ABIO, name='ABIO'),
-    url('register/', user_views.register, name='register'), 
-
+    url('register/', user_views.register, name='register'),
+    url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), 
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
 ]
